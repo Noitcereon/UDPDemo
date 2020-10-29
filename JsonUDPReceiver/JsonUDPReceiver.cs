cb83b8a97aabb1ee273676de6217f9c3c24ed8c8
Thomas (Noit) <40148361+Noitcereon@users.noreply.github.com>
2020-10-29 11:53:31 +0100

    Begun JSON UDP.

---
+++ JsonUDPReceiver/JsonUDPReceiver.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace JsonUDPReceiver
+{
+    public class JsonUDPReceiver
+    {
+        public void Start()
+        {
+            UdpClient client = new UdpClient(7765);
+
+            while (true)
+            {
+                // TODO: Complete at home (no json here yet)
+
+                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0); // values are overriden when it receives data.
+                byte[] buffer = client.Receive(ref remoteEndPoint);
+                string receivedMessage = Encoding.UTF8.GetString(buffer);
+
+                Console.WriteLine(receivedMessage);
+
+                byte[] response = Encoding.UTF8.GetBytes($"\"{receivedMessage}\" received");
+                client.Send(response, response.Length, remoteEndPoint);
+            }
+        }
+    }
+}
---
+++ JsonUDPSender/JsonUDPSender.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JsonUDPSender
+{
+    public class JsonUDPSender
+    {
+        public void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
