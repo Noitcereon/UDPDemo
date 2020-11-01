8ca0475c3fb696d63ce8daf0c86ccef2641557cd
Thomas (Noit) <tba@live.dk>
2020-11-01 20:59:44 +0100

    Added comment explaining broadcast.

--- JsonUDPSender/JsonUDPSender.cs
+++ JsonUDPSender/JsonUDPSender.cs
@@ -18,6 +18,7 @@ namespace JsonUDPSender
             string message = JsonSerializer.Serialize(car);
             byte[] bytesToSend = Encoding.UTF8.GetBytes(message);
 
+            // To broadcast on the local network on port 7765 replace the IPAddress.Loopback with IPAddress.Broadcast
             IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Loopback, 7765);
             client.Send(bytesToSend, bytesToSend.Length, remoteEndPoint);
             Console.WriteLine(message + " ---  was sent");
